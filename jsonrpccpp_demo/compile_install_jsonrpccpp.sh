6e5d6a63539498155de8c872f9889b85a36da3d1
veraoks <venkatakrishnarao.ks@gmail.com>
2016-07-25 01:00:08 +0530

    The following are successful.  1. provision new vm using Vagrant.  2. Use Vagrant with Ansible as provisioner to install httpd, and the other misc packages using yum module.  3. Use Vagrant with shell provisioner to compile jsonrpccpp on the guest vm.

---
+++ jsonrpccpp_demo/compile_install_jsonrpccpp.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+	# 
+	cd /var/tmp
+
+	# 1:	
+	#
+	echo "INSTALLING dependency jsoncpp.."
+	git clone https://github.com/open-source-parsers/jsoncpp.git
+	cd jsoncpp/
+	mkdir -p build && cd build && cmake -DCMAKE_CXX_FLAGS="-fPIC" ../ && make && make install
+	rc=$?
+	if [[ $rc != 0 ]]; then
+		echo "FAILED to install jsoncpp. This is unexpected. Contact the owner of the Vagrant file fo help."
+		exit $rc
+	fi
+	echo "INSTALLING dependency jsoncpp.. Done"
+
+	# 2:
+	#	
+	cd /var/tmp
+	echo "INSTALLING dependency argtables2-13.."
+	output=argtable.tar.gz
+
+	#wget -q https://sourceforge.net/projects/argtable/files/argtable/argtable-2.13/argtable2-13.tar.gz/download -O $output
+	tar xf $output
+	cd argtable2-13 && ./configure && make && make install
+	rc=$?
+	if [[ $rc != 0 ]]; then
+		echo "FAILED to install argtable. This is unexpected. Contact the owner of the Vagrant file fo help."
+		exit $rc
+	fi
+	echo "INSTALLING dependency argtables2-13.. Done"
+
+	git clone git://github.com/cinemast/libjson-rpc-cpp.git
+
+
+	# 3:
+	#
+	cd /var/tmp
+	echo "INSTALLING dependency jsonrpccpp.."
+	git clone https://github.com/cinemast/libjson-rpc-cpp.git
+	cd libjson-rpc-cpp
+	git checkout v0.5.0
+	mkdir -p build && cd build && cmake ../ && make && make install
+	rc=$?
+	if [[ $rc != 0 ]]; then
+		echo "FAILED to install json-rpc-cpp. This is unexpected. Contact the owner of the Vagrant file fo help."
+		exit $rc
+	fi
+	echo "INSTALLING dependency jsoncpp.. Done"
